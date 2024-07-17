dca74b35c8188a8154439fb0c928f496db7a38ef
Abbas <abbas_kanj@hotmail.com>
2024-07-17 14:28:39 +0300

    used the react router for navigation

--- music-player/src/App.jsx
+++ music-player/src/App.jsx
@@ -1,9 +1,21 @@
-function App() {
-  return (
-    <>
-      <h1 className="text-red-100">hello</h1>
-    </>
-  );
-}
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
 
+const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
+};
 export default App;
