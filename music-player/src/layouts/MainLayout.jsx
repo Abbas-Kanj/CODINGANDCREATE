abef78a58dc1ea0c386a7c483fb75f81d53d0716
Abbas <abbas_kanj@hotmail.com>
2024-07-17 14:29:33 +0300

    created main layout

---
+++ music-player/src/layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import AudioPlayer from "../components/AudioPlayer";
+
+const MainLayout = () => {
+  return (
+    <div className="w-screen h-screen flex flex-col p-4 gap-1">
+      <Sidebar />
+      <AudioPlayer />
+    </div>
+  );
+};
+
+export default MainLayout;
