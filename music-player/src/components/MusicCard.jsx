3a785c73f91461ac63c24f9e9a5edbcbab850936
Abbas <abbas_kanj@hotmail.com>
2024-07-27 14:32:27 +0300

    added anothre music card to test ,multiple audio sources

--- music-player/src/components/MusicCard.jsx
+++ music-player/src/components/MusicCard.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import image from "../assets/images/cardib.png";
 import { MdPlayArrow } from "react-icons/md";
 import audio1 from "../assets/audio/NEFFEX - Badass [Lyric Video].m4a";
-import { Counter } from "../features/counter/counter";
+import audio2 from "../assets/audio/AsYouFadeAway.mp3";
 import { useDispatch } from "react-redux";
 import { setCurrentTrack } from "../features/track/trackSlice";
 const MusicCard = () => {
@@ -22,7 +22,20 @@ const MusicCard = () => {
         />
         <h2 className="font-semibold">Invation of privacy</h2>
         <small>Cardi B.</small>
-        <Counter />
+      </div>
+      <div className="flex flex-col text-start gap-1 group relative">
+        <img
+          src={image}
+          alt="Song Image"
+          className="w-56 h-auto object-cover rounded-2xl shadow-xl"
+        />
+        <MdPlayArrow
+          className="group-hover:block hidden fill-blue-500 size-11 absolute m-24
+         bg-white bg-opacity-80 rounded-full hover:fill-blue-300 hover:cursor-pointer"
+          onClick={() => dispatch(setCurrentTrack(audio2))}
+        />
+        <h2 className="font-semibold">Invation of privacy</h2>
+        <small>Cardi B.</small>
       </div>
     </>
   );
