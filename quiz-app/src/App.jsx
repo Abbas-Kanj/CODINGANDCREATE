474624a423195cccf2a3f96c67e3608c06f0d960
Abbas <abbas_kanj@hotmail.com>
2024-07-28 19:02:47 +0300

    added main section to the app

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,11 +1,11 @@
-import HeroSection from "./components/HeroSection";
+import MainSection from "./components/MainSection";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <HeroSection />
+      <MainSection />
     </>
   );
 };
