ab0ba769f8c4a83ef9f27f3e62259226fc177663
Abbas <abbas_kanj@hotmail.com>
2024-07-29 15:08:15 +0300

    removed console log from main section/ named the quiz.json array

--- quiz-app/src/components/MainSection.jsx
+++ quiz-app/src/components/MainSection.jsx
@@ -4,7 +4,6 @@ import QuizBox from "./QuizBox";
 
 const MainSection = () => {
   const [showQuizBox, setShowQuizBox] = useState(false);
-  console.log(showQuizBox);
   return (
     <>
       {showQuizBox ? (
