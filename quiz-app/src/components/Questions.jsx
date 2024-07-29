23840ed733b391f31341baa5eba94907dcdbd653
Abbas <abbas_kanj@hotmail.com>
2024-07-29 15:07:26 +0300

    modified the questions component to render questions when question is submitted

--- quiz-app/src/components/Questions.jsx
+++ quiz-app/src/components/Questions.jsx
@@ -1,55 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Questions = () => {
+const Questions = ({
+  index,
+  questions,
+  setIsAnswered,
+  setIsAnsweredCorrectly,
+}) => {
+  const { question, answers, hint, correct_answer } = questions;
+  const [radioValue, setRadioValue] = useState("");
+
+  const handleAnswer = (e) => {
+    e.preventDefault();
+    setIsAnswered(true);
+    if (radioValue === correct_answer) {
+      setIsAnsweredCorrectly(true);
+    } else {
+      setIsAnsweredCorrectly(false);
+    }
+  };
+
   return (
-    <>
+    <section key={index}>
       <div className="flex justify-between">
-        <h1 className="prose-2xl text-secondary font-semibold">
-          What is the country that...
-        </h1>
-        <div className="tooltip" data-tip="hello">
+        <h1 className="prose-2xl text-secondary font-semibold">{question}</h1>
+        <div className="tooltip" data-tip={hint}>
           <button className="btn rounded-full text-primary">Hint</button>
         </div>
       </div>
-      <div className="form-control">
-        <label className="label cursor-pointer flex justify-start gap-5">
-          <input
-            type="radio"
-            name="radio-10"
-            className="radio checked:bg-primary"
-            defaultChecked
-          />
-          <span className="label-text">Red pill</span>
-        </label>
-        <label className="label cursor-pointer flex justify-start gap-5">
-          <input
-            type="radio"
-            name="radio-10"
-            className="radio checked:bg-primary"
-            defaultChecked
-          />
-          <span className="label-text">Red pill</span>
-        </label>
-        <label className="label cursor-pointer flex justify-start gap-5">
-          <input
-            type="radio"
-            name="radio-10"
-            className="radio checked:bg-primary"
-            defaultChecked
-          />
-          <span className="label-text">Red pill</span>
-        </label>
-        <label className="label cursor-pointer flex justify-start gap-5">
-          <input
-            type="radio"
-            name="radio-10"
-            className="radio checked:bg-primary"
-            defaultChecked
-          />
-          <span className="label-text">Red pill</span>
-        </label>
-      </div>
-    </>
+      <form onSubmit={handleAnswer} className="form-control">
+        {answers.map((answer, i) => (
+          <label
+            key={i}
+            className="label cursor-pointer flex justify-start gap-5"
+          >
+            <input
+              type="radio"
+              name="radio-10"
+              className="radio checked:bg-primary"
+              checked={radioValue === answer}
+              value={answer}
+              onChange={() => setRadioValue(answer)}
+            />
+            <span className="label-text">{answer}</span>
+          </label>
+        ))}
+        <button type="submit" className="btn mt-5">
+          Next Question
+        </button>
+      </form>
+    </section>
   );
 };
 
